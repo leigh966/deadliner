ea07f7648135330eb275cfd090ace213b4c4afd8
leigh966 <leigh966@hotmail.com>
2024-12-06 22:30:47 +0000

    changed from an unordered list to a table with some data formatting that can handle the updated fields

--- src/app/DeadlineList.js
+++ src/app/DeadlineList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pool from "../lib/db";
+import styles from "./DeadlineList.module.css";
 
 // This is a Server Component in Next.js that queries the PostgreSQL database
 async function fetchData() {
@@ -19,17 +20,48 @@ export default async function DeadlineList() {
   const deadlines = await fetchData();
 
   return (
-    <ul>
-      {deadlines.length > 0 ? (
-        deadlines.map((dl) => (
-          <li key={dl.id}>
-            <h3>{dl.title}</h3>
-            <p>{dl.deadline.toString()}</p>
-          </li>
-        ))
-      ) : (
-        <p>No deadlines available.</p>
-      )}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th className={styles.title}>Title</th>
+          <th className={styles.description}>Description</th>
+          <th className={styles.date}>Start Date</th>
+          <th className={styles.date}>End Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {deadlines.length > 0 ? (
+          deadlines.map((dl) => (
+            <tr key={dl.title}>
+              <td className={styles.title}>{dl.title}</td>
+              <td className={styles.description}>{dl.description}</td>
+              <td className={styles.date}>
+                {dl.start_date.toString().substring(0, 15)}
+              </td>
+              <td className={styles.date}>
+                {dl.end_date.toString().substring(0, 15)}
+              </td>
+            </tr>
+          ))
+        ) : (
+          <p>No deadlines available.</p>
+        )}
+      </tbody>
+    </table>
   );
+
+  // return (
+  //   <ul>
+  //     {deadlines.length > 0 ? (
+  //       deadlines.map((dl) => (
+  //         <li key={dl.id}>
+  //           <h3>{dl.title}</h3>
+  //           <p>{dl.deadline.toString()}</p>
+  //         </li>
+  //       ))
+  //     ) : (
+  //       <p>No deadlines available.</p>
+  //     )}
+  //   </ul>
+  // );
 }
