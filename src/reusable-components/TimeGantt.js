6c692468f8d091e139b033d3ba4a7c0bbe6d4844
leigh966 <leigh966@hotmail.com>
2024-12-29 22:26:01 +0000

    added basic time annotation

--- src/reusable-components/TimeGantt.js
+++ src/reusable-components/TimeGantt.js
@@ -42,11 +42,48 @@ function drawBars(context, data, padding, timeToScreenMultiplier, globalStart) {
       record,
       timeToScreenMultiplier,
       globalStart,
-      parseInt(padding)
+      padding
     );
     console.log(xDims);
-    context.fillRect(xDims.x, index * 50 + parseInt(padding), xDims.width, 40);
+    context.fillRect(xDims.x, index * 50 + padding, xDims.width, 40);
   });
+}
+
+function getShortDate(date) {
+  return (
+    date.getDate() +
+    "/" +
+    (parseInt(date.getMonth()) + 1) +
+    "/" +
+    date.getFullYear()
+  );
+}
+
+function drawAnnotations(context, minDate, maxDate, width, height, padding) {
+  let notch_thickness = 3;
+  let notch_height = 10;
+  let line_thickness = 3;
+  let line_bottom = 0;
+  let notch_y =
+    height - padding - line_bottom - line_thickness / 2 - notch_height / 2;
+  let line_y = height - padding - (line_bottom + line_thickness);
+  let labelWidth = 40;
+
+  context.fillRect(padding, line_y, width - padding * 2, 3);
+  context.fillRect(padding, notch_y, notch_thickness, notch_height); // start notch
+  context.fillRect(width - padding - 3, notch_y, notch_thickness, notch_height); // end notch
+  context.fillText(
+    getShortDate(minDate),
+    padding - labelWidth / 2,
+    line_y - 20,
+    labelWidth
+  );
+  context.fillText(
+    getShortDate(maxDate),
+    width - padding - labelWidth / 2,
+    line_y - 20,
+    labelWidth
+  );
 }
 
 export default function TimeGantt(props) {
@@ -57,9 +94,10 @@ export default function TimeGantt(props) {
 
   const startDate = getStart(props.data);
   const endDate = getEnd(props.data);
-
+  const BUILT_IN_PADDING = 50;
+  const totalPadding = parseInt(props.padding) + BUILT_IN_PADDING;
   const timeToScreenMultiplier =
-    (props.width - props.padding * 2) /
+    (props.width - totalPadding * 2) /
     (endDate.getTime() - startDate.getTime());
 
   console.log(timeToScreenMultiplier);
@@ -72,9 +110,18 @@ export default function TimeGantt(props) {
     drawBars(
       context,
       props.data,
-      parseInt(props.padding),
+      totalPadding,
       timeToScreenMultiplier,
       startDate
+    );
+
+    drawAnnotations(
+      context,
+      startDate,
+      endDate,
+      parseInt(props.width),
+      parseInt(props.height),
+      totalPadding
     );
   });
 
