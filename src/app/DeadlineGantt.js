25873f991f22e90d37eab01a1436dcce9276c303
leigh966 <leigh966@hotmail.com>
2025-02-17 16:55:46 +0000

    updated deadline list to make edit form appear when a bar is clicked

--- src/app/DeadlineGantt.js
+++ src/app/DeadlineGantt.js
@@ -1,24 +1,37 @@
 "use client";
 
 import TimeGantt from "@/reusable-components/TimeGantt/TimeGantt";
+import { useState } from "react";
+import EditDeadlineForm from "./EditDeadlineForm";
 
 export default function DeadlineGantt({ deadlines }) {
+  const [record, setRecord] = useState(false);
+  console.log(deadlines[0]);
   return (
-    <TimeGantt
-      width="80%"
-      height="40%"
-      maxBarHeight={100}
-      data={deadlines.map((dl) => {
-        return {
-          startDate: dl.start_date,
-          endDate: dl.end_date,
-          label: dl.title,
-        };
-      })}
-      onBarClicked={(record) => {
-        console.log(record.label);
-      }}
-      noDataMessage="No Deadlines Yet"
-    />
+    <>
+      <TimeGantt
+        width="80%"
+        height="40%"
+        maxBarHeight={100}
+        data={deadlines.map((dl) => {
+          return {
+            startDate: dl.start_date,
+            endDate: dl.end_date,
+            label: dl.title,
+            recordId: dl.id,
+          };
+        })}
+        onBarClicked={(record) => {
+          console.log(record.label);
+          setRecord(
+            deadlines.find((dl) => {
+              return dl.id == record.recordId;
+            })
+          );
+        }}
+        noDataMessage="No Deadlines Yet"
+      />
+      {record && <EditDeadlineForm setVisible={setRecord} record={record} />}
+    </>
   );
 }
