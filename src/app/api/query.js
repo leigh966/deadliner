2faf97862bbb20c36a6e1dd50cf3d181798053d1
leigh966 <leigh966@hotmail.com>
2025-03-07 12:06:12 +0000

    updated security of endpoint by not processing NULL or empty cookies

--- src/app/api/query.js
+++ src/app/api/query.js
@@ -7,9 +7,10 @@ export async function runQuery(query, values) {
 }
 
 export async function getUserId(session) {
-  let result = await runQuery("SELECT id FROM users WHERE cookie=$1", [
-    session,
-  ]);
+  let result = await runQuery(
+    "SELECT id FROM users WHERE cookie=$1 AND cookie!='' AND cookie IS NOT NULL",
+    [session]
+  );
   console.log("id fetched");
   console.log(result.rows);
   return result.rows[0].id;
